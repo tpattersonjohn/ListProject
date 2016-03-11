c722ba55ac732f549ea702453163cb2cb8d54cc2
John Patterson <john.patterson@outlook.com>
2016-03-10 16:24:51 -0800

    Added sort() method
    Just a basic bubblesort

--- src/List.java
+++ src/List.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
 
-public class List<T> {
+public class List<T extends Comparable<T>> {
 
 	private Object[] elementData;
 	private int size;
+
+	
 	
 	public List(){
 		this(32);
@@ -63,6 +65,39 @@ public class List<T> {
 	private void rangeCheck(int index){
 		if (index >= size)
 			throw new IndexOutOfBoundsException();
+	}
+
+	@SuppressWarnings("unchecked")
+	public void sort(){
+		
+		boolean didSort = true;
+		
+		while(didSort){
+			didSort = false;
+			for(int i = 0; i < this.size - 1; i++){
+				
+				T e1 = (T) this.elementData[i];
+				T e2 = (T) this.elementData[i+1];
+				
+				if(e1.compareTo(e2) > 0){
+					T temp = e1; 
+					elementData[i] = elementData[i+1];
+					elementData[i+1] = temp;
+					didSort = true;
+				}
+				
+			}
+			
+		}
+		
+				
+	}
+	
+	
+	public void printArrayHelper(Object[] a){
+		for(int i = 0; i<a.length; i++){
+			System.out.println("[" + i + "]:" + a[i]);
+		}
 	}
 	
 	
--- src/ListProject.java
+++ src/ListProject.java
@@ -4,18 +4,22 @@ public class ListProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int listSize = 10000000;
 		
 		System.out.println("Creating new list...");
-		List<Integer> list = new List<Integer>();
+		List<Integer> list = new List<Integer>(listSize);
 		
 		System.out.println("Current capacity of list: " + list.ensureCapacity(0));
 		
-		for(int i = 0; i < 10000; i++){
+		for(int i = listSize - 1; i >= 0; i--){
 			list.add(i);
 		}
 		
 		System.out.println("Current elements in list: " + list.size());
-		System.out.println("Current capacity of list: " + list.ensureCapacity(0));
+		
+		
+		System.out.println("Sorting the list...");
+		list.sort();
 		
 		System.out.println("End of program.");
 	
