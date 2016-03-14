3f6bfe1a772b4f041f9127e929ea513513f0af55
John Patterson <john.patterson@outlook.com>
2016-03-14 16:59:26 -0700

    Cleaning up ListProject

--- src/main/java/ListProject.java
+++ src/main/java/ListProject.java
@@ -4,44 +4,7 @@ public class ListProject {
 
 	public static void main(String[] args) {
 
-		int listSize = 10000;
-		StopWatch s = new StopWatch();
-
-		System.out.println("Creating new list...");
-		List<Integer> list = new List<Integer>();
-
-		for (int i = listSize - 1; i >= 0; i--) {
-			list.add(i);
-		}
-
-		System.out.println("-----------TEST1: FAST------------");
-		System.out.println("List sort status: " + list.isSorted());
-		System.out.print("Sorting the list...");
-		s.Start();
-		list.sort();
-		s.Stop();
-
-		System.out.println("Sorted.");
-		System.out.println("Time: " + s.getTimeInMicroSec());
-		System.out.println("List sort status: " + list.isSorted());
-
-		list = new List<Integer>();
-
-		for (int i = listSize - 1; i >= 0; i--) {
-			list.add(i);
-		}
-
-		System.out.println("-----------TEST2: SLOW------------");
-
-		s.reset();
-		System.out.println("List sort status: " + list.isSorted());
-		System.out.print("Sorting the list...");
-		s.Start();
-		list.slowsort();
-		s.Stop();
-		System.out.println("Sorted.");
-		System.out.println("Time: " + s.getTimeInMicroSec());
-		System.out.println("List sort status: " + list.isSorted());
+		System.out.println("Launching program...");
 
 		System.out.println("End of program.");
 
