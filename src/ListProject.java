01eeb883a9984b764f1f8bf207a84eeeff115ebb
John Patterson <john.patterson@outlook.com>
2016-03-14 12:57:52 -0700

    Updated POM
    Updated POM again. Also some code formatting.

--- src/ListProject.java
+++ src/ListProject.java
@@ -2,55 +2,48 @@
 public class ListProject {
 
 	public static void main(String[] args) {
-	
-		int listSize = 100000;		
+
+		int listSize = 100000;
 		StopWatch s = new StopWatch();
-		
-		
-		
+
 		System.out.println("Creating new list...");
 		List<Integer> list = new List<Integer>();
-			
-		for(int i = listSize - 1; i >= 0; i--){
+
+		for (int i = listSize - 1; i >= 0; i--) {
 			list.add(i);
 		}
-		
+
 		System.out.println("-----------TEST1: FAST------------");
 		System.out.println("List sort status: " + list.isSorted());
 		System.out.print("Sorting the list...");
 		s.Start();
 		list.sort();
 		s.End();
-		System.out.println("Sorted.");		
+
+		System.out.println("Sorted.");
 		System.out.println("Time: " + s.getTimeInMicroSec());
 		System.out.println("List sort status: " + list.isSorted());
-		
 
 		list = new List<Integer>();
-		
-		for(int i = listSize - 1; i >= 0; i--){
+
+		for (int i = listSize - 1; i >= 0; i--) {
 			list.add(i);
 		}
-		
+
 		System.out.println("-----------TEST2: SLOW------------");
-		
+
 		s.reset();
 		System.out.println("List sort status: " + list.isSorted());
 		System.out.print("Sorting the list...");
 		s.Start();
 		list.slowsort();
 		s.End();
-		System.out.println("Sorted.");		
+		System.out.println("Sorted.");
 		System.out.println("Time: " + s.getTimeInMicroSec());
 		System.out.println("List sort status: " + list.isSorted());
-		
-		
-		
-		
+
 		System.out.println("End of program.");
-	
-		
-		
+
 	}
 
 }
